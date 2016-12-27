d20e4e8179790a70f3743b4494b8fd955d7feeca
Haut-Stone <haut.1604.stone@gmail.com>
2016-12-27 23:38:42 +0800

    今天看了python入门，掌握了一点基础语法
    python确实有很多方便支持，希望以后可以善加利用

---
+++ first_test.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Author: Haut-Stone
+# @Date:   2016-12-27 23:37:13
+# @Last Modified by:   Haut-Stone
+# @Last Modified time: 2016-12-27 23:37:27
+
+import math
+def area(radius):
+	""" Return the area of a circle with the given redius.
+	For example:
+	>>> area(5.5)
+	95.0333333
+	"""
+	return math.pi * radius ** 2
