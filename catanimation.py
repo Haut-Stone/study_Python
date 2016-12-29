0abf1b4616aa373bbab7143dc792df887240706b
Haut-Stone <haut.1604.stone@gmail.com>
2016-12-29 23:21:34 +0800

    一些python游戏的基础
    对python的函数库饮用大概有个一个初步认识，感觉还是蛮方便的

---
+++ catanimation.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+# @Author: Haut-Stone
+# @Date:   2016-12-29 21:08:30
+# @Last Modified by:   Haut-Stone
+# @Last Modified time: 2016-12-29 22:05:18
+
+# 导入库
+import pygame , sys
+from pygame.locals import *
+
+#创建背景
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((500, 500), 0, 32)
+pygame.display.set_caption('hello world')
+
+#定义常量，变量
+FPS = 60
+WHITE = (255, 255, 255)
+fpsClock = pygame.time.Clock()
+catImg = pygame.image.load('cat.png')#加载图片
+catx = 10
+caty = 10
+direction = 'right'
+
+#动作变换循环
+while True:
+	DISPLAYSURF.fill(WHITE)
+
+	if direction == 'right':
+		catx += 2
+		if catx == 280:
+			direction = 'down'
+	elif direction == 'down':
+		caty += 10
+		if caty == 220:
+			direction = 'left'
+	elif direction == 'left':
+		catx -= 3
+		if catx == 10:
+			direction = 'up'
+	elif direction == 'up':
+		caty -= 10
+		if caty == 10:
+			direction = 'right'
+	
+	DISPLAYSURF.blit(catImg, (catx, caty))#将图片显示在画布上
+
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			pygame.quit()
+			sys.exit()
+
+	pygame.display.update()
+	fpsClock.tick(FPS)#改变程序运行的帧率
---
+++ fonttext.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# @Author: Haut-Stone
+# @Date:   2016-12-29 21:54:08
+# @Last Modified by:   Haut-Stone
+# @Last Modified time: 2016-12-29 22:14:03
+
+import pygame , sys
+from pygame.locals import *
+
+pygame.init()
+
+DISPLAYSURF = pygame.display.set_mode((500, 500), 0, 32)
+pygame.display.set_caption('fonttext')
+
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 128)
+C1 = (63, 161, 207)
+C2 = (245, 206, 57)
+
+fronObj = pygame.font.Font('freesansbold.ttf', 64)#传入python的一种自带字体，和字体的大小。
+textSurfaceObj = fronObj.render('hello world', True, C1, C2)#字符，是否抗锯齿，文本颜色，背景颜色
+textRectObj = textSurfaceObj.get_rect()#?
+textRectObj.center = (250, 250)#设定中心位置
+
+while True:
+	DISPLAYSURF.fill(WHITE)
+	DISPLAYSURF.blit(textSurfaceObj, textRectObj)#载入画布
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			pygame.quit()
+			sys.exit
+	pygame.display.update()
